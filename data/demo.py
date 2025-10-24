cf97f4d48f7f90bcdca57a460d3fcd1f7f3eb14a
SergioMartinezRico <sergiomartinezrico@gmail.com>
2025-10-24 06:32:38 +0200

    juego y demo definitivas

--- data/demo.py
+++ data/demo.py
@@ -1,136 +1,18 @@
-from utils import Tablero
-import random
+import utils as ut
+print ("\n🚢🌊⚓️==================================================⚓️🌊🚢")
+print ("==================    HUNDIR LA FLOTA    ==================")
+print ("==================  ¡A LA BATALLA NAVAL! ⚔️  ==================")
+print ("🚢🌊⚓️==================================================⚓️🌊🚢\n")
 
-
-#funciones
-
-#match letras numeros
-
-
-def match_letras(letra):
-    """Convierte letra A-J en índice 0-9"""
-    dicc_col = {"A":0,"B":1,"C":2,"D":3,"E":4,"F":5,"G":6,"H":7,"I":8,"J":9}
-    letra = letra.upper()
-    if letra in dicc_col:
-        return dicc_col[letra]
-    else:
-        return -1  # fuera de rangom
-    
-def match_numeros(numero): #para la demo
-    """Convierte letra A-J en índice 0-9"""
-    dicc_col = {0:"A",1:"B",2:"C",3:"D",4:"E",5:"F",6:"G",7:"H",8:"I",9:"J"}
-    if numero in dicc_col:
-        return dicc_col[numero]
-    
-    
-
-def pedir_coordenadas():
-    """Pide al jugador fila y columna válidas"""
-    valido = False
-    while valido == False:
-        try:
-            print("Dispara")
-            fila = int(input("Dime una fila (1-10): ")) - 1
-            col = input("Dime una columna (A-J): ")
-            col = match_letras(col)
-            if fila < 0 or fila > 9 or col == -1: #comprobamos si no estan en el rango
-                print("Coordenadas fuera de rango. Intenta de nuevo.")
-            else:
-                valido = True
-        except ValueError:
-            print("Entrada no válida. Intenta de nuevo.")
-    return fila, col
-
-def turno_jugador(tablero_enemigo):
-    """Turno del jugador:
-     -pìde coordenadas con la funcion 
-     -usa el metodo disparar de tablero
-     -con el resultado, decide si tocado o agua
-     -comprueba si fuera o repetido
-     -se basa en una variable jugar que cuando pasa a false acaba el turno
-       """
+tipo_juego = input("¿Quiere jugar una partida o una demo? Introduzca Partida o Demo:  ")
+jugar = False
+while jugar == False:
+  if tipo_juego.lower() == "partida":
+    ut.jugar_partida()
     jugar = True
-    while jugar == True:
-        fila, col = pedir_coordenadas()
-        resultado = tablero_enemigo.disparar(fila, col)
-        
-        if resultado == "tocado":
-            print("Has tocado un barco!")
-            tablero_enemigo.mostrar()
-            # si todos los barcos del enemigo están hundidos, termina el juego
-            if tablero_enemigo.todos_hundidos():
-                return "ganado" #se acaba el while
-            # si no, el jugador sigue disparando
-
-        elif resultado == "hundido":
-          print("Has hundido un barco!")
-          tablero_enemigo.mostrar()
-          # si todos los barcos del enemigo están hundidos, termina el juego
-          if tablero_enemigo.todos_hundidos():
-              return "ganado" #se acaba el while
-          # si no, el jugador sigue disparando
-
-        elif resultado == "agua":
-            print("Has fallado. Agua.")
-            tablero_enemigo.mostrar()
-            jugar = False  # termina el turno del jugador
-
-        elif resultado == "repetido":
-            print("Ya habías disparado ahí. Prueba otra vez.")
-
-        elif resultado == "fuera":
-            print("Coordenadas fuera del tablero. Prueba otra vez.")
-
-def turno_enemigo(tablero_jugador):
-    """Turno de el enemigo
-     -crea coordenadas random
-     -usa el metodo disparar de tablero
-     -con el resultado, decide si tocado o agua
-     -comprueba si fuera o repetido
-     -se basa en una variable jugar que cuando pasa a false acaba el turno
-    """
+  elif tipo_juego.lower() == "demo":
+    ut.jugar_demo()
     jugar = True
-    while jugar == True:
-        fila = random.randint(0, 9)
-        col = random.randint(0, 9)
-        resultado = tablero_jugador.disparar(fila, col)
-        letra = match_numeros(col)
-        numero = fila + 1
-
-        if resultado == "tocado":
-            print(f"el enemigo dispara a {numero}{letra}: Tocado!")
-            tablero_jugador.mostrar()
-            # si todos los barcos del jugador están hundidos, termina el juego
-            if tablero_jugador.todos_hundidos():
-                return "perdido" #se acaba el while
-            # si no, el enemigo sigue disparando
-        
-        elif resultado == "hundido":
-          print("Has hundido un barco!")
-          tablero_jugador.mostrar()
-          # si todos los barcos del enemigo están hundidos, termina el juego
-          if tablero_jugador.todos_hundidos():
-              return "perdido" #se acaba el while
-          # si no, el jugador sigue disparando
-
-        elif resultado == "agua":
-            print(f"el enemigo dispara a {numero}{letra}: Agua.")
-            tablero_jugador.mostrar()
-            jugar = False  # termina el turno de el enemigo
-
-        elif resultado == "repetido":
-            print(f"el enemigo dispara a {numero}{letra}: Repetido.")
-            # intenta otra vez con nuevas coordenadas
-            continue
-
-        elif resultado == "fuera":
-            print(f"el enemigo dispara a {numero}{letra}: Incorrecto.")
-            # intenta otra vez
-            continue
-
-#juego
-
-
-
-
-jugar_partida()
+  else:
+    tipo_juego = input ("Entrada erronea. Introduzca Partida o Demo:  ")
+    jugar = False
--- data/juego.py
+++ data/juego.py
@@ -4,15 +4,4 @@ print ("==================    HUNDIR LA FLOTA    ==================")
 print ("==================  ¡A LA BATALLA NAVAL! ⚔️  ==================")
 print ("🚢🌊⚓️==================================================⚓️🌊🚢\n")
 
-tipo_juego = input("¿Quiere jugar una partida o una demo? Introduzca Partida o Demo:  ")
-jugar = False
-while jugar == False:
-  if tipo_juego.lower() == "partida":
-    ut.jugar_partida()
-    jugar = True
-  elif tipo_juego.lower() == "demo":
-    ut.jugar_demo()
-    jugar = True
-  else:
-    tipo_juego = input ("Entrada erronea. Introduzca Partida o Demo:  ")
-    jugar = False
+ut.jugar_partida()
